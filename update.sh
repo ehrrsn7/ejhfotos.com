5328605843d5f6bb537f1bbf111c9b657e25814f
ehrrsn7 <har16020@byui.edu>
2022-12-23 23:44:52 +0000

    got everything good on the linux side

--- update.sh
+++ update.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-cd /Users/ehrrsn7/Documents/Code/PERN/ejhfotos.com/ || echo "directory not found"
+cd /Users/ehrrsn7/Documents/Code/PERN/ejhfotos.com/ 2> "" || echo "directory not found"
 
 # define
 function update_node_app {
-   app="$1"
-   cd "$app/" || (echo "missing $app/ directory"; exit 1)
+   APP="$1"
+   cd "$APP/" || (echo "missing $APP/ directory"; exit 1)
    mkdir -p logs/; touch "logs/npm.txt"
    npm i -q > "logs/npm.txt" || (echo "npm error"; exit 1)
    cd ..
 }
 
 function update_react_build {
-   app="$1"
-   cd "$1" || (echo "missing $app/ directory"; exit 1)
+   APP="$1"
+   cd "$1" || (echo "missing $APP/ directory"; exit 1)
    touch "logs/react-build.txt"
    npm run build > "react-build.txt"
    cd ..
@@ -30,6 +30,9 @@ function update_react_app {
    update_react_build "$1"
 }
 
+# temporarily stop server
+pm2 stop server || echo "(Ignore this if not on production server.)"
+
 # pull changes from repository
 git pull
 
@@ -40,5 +43,11 @@ update_express_app "server"
 update_react_app "app"
 update_react_app "about"
 update_react_app "linktree"
+
+# refresh nginx reverse proxy host
+systemctl restart nginx || echo "Unable to restart nginx. (Ignore if not running on production server on linux)."
+
+# start server up again
+pm2 start server || echo "(Ignore if not on production server.)"
 
 echo "Updates finished successfully."
