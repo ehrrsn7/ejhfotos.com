0bc6bec81d0f2043cc7eb7d0e5d32927e479691d
E Harrison <har16020@byui.edu>
2022-12-23 14:59:21 -0800

    added about/ react app

--- server/server.js
+++ server/server.js
@@ -11,11 +11,5 @@ const linktree = listen(83)
 
 // use middleware to serve all react apps
 www.use(express.static("./app/build"))
-about.use(express.static("./app/build"))
-linktree.use(express.static("./app/build"))
-
-// TODO: use http://api.ejhfotos.com/ as ./api on port 3000
-// TODO: use http://linktree.ejhfotos.com/ as ./linktree "
-// TODO: use http://portfolio.ejhfotos.com/ as ./portfolio "
-// TODO: use http://minecraft.ejhfotos.com/ as ./minecraft on port 25565?
-// https://www.digitalocean.com/community/questions/how-do-i-point-my-custom-domain-to-my-ip-port-41-111-20-36-8080
+about.use(express.static("./about/build"))
+linktree.use(express.static("./linktree/build"))
--- update.sh
+++ update.sh
@@ -3,28 +3,33 @@
 cd /Users/ehrrsn7/Documents/Code/PERN/ejhfotos.com/ || echo "directory not found"
 
 # define
-logfile="logs/npm.txt"
-function makelogdir() {
-   mkdir -p logs/;
-   touch "$logfile"
+function update_node_app {
+   app="$1"
+   cd "$app/" || (echo "missing $app/ directory"; exit 1)
+   mkdir -p logs/; touch "logs/npm.txt"
+   npm i -q > "logs/npm.txt" || (echo "npm error"; exit 1)
+   cd ..
+}
+
+function update_express_app {
+   echo "updating express application: $1/"
+   update_node_app "$1"
+}
+
+function update_react_app {
+   echo "updating react application: $1/"
+   update_node_app "$1"
 }
 
 # pull changes from repository
 git pull
 
 # update server
-echo "updating express application..."
-cd server/ || echo "missing server/ directory"; exit 1
-makelogdir
-npm i -q > logfile || echo "npm error"; exit 1
-cd ..
+update_express_app "server"
 
-# update react application
-echo "updating react application..."
-cd app/ || echo "missing app/ directory"; exit 1
-makelogdir
-npm i -q > logfile || echo "npm error"; exit 1
-npm run build -q > logs/react-build.txt || echo "react build error"; exit 1
-cd ..
+# update react applications
+update_react_app "app"
+update_react_app "about"
+update_react_app "linktree"
 
 echo "Updates finished successfully."
