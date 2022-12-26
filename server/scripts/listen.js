ce436e94ccf38d890f4d6667a76904d615a7fe95
E Harrison <har16020@byui.edu>
2022-12-26 15:45:25 -0800

    sdfkjsd

--- server/scripts/listen.js
+++ server/scripts/listen.js
@@ -4,15 +4,15 @@ const express = require("express")
 // import my scripts
 const { getIPAddress } = require("./getIPAddress")
 
-function listen(port) {
+function listen(port, subdomain="") {
    const server = express()
 
    server.listen(port, () => {
-      console.log(`App listening on port ${port}:\n`)
+      console.log(`'${subdomain}' App listening on port ${port}:\n`)
       console.log(`\thttp://localhost:${port}/`)
       if (getIPAddress()) {
          console.log(`\thttp://${getIPAddress()}:${port}/`)
-         console.log(`\thttp://ejhfotos.com/\n`)
+         console.log(`\thttp://${subdomain}.ejhfotos.com/\n`)
       }
    })
 
