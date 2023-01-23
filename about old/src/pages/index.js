d43e86b7c45fae070a332c61f36813e2e32f8b18
E Harrison <har16020@byui.edu>
2023-01-22 16:44:25 -0800

    sdlkf

--- about old/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Main } from './Main/Main'
-export { default as PageNotFound } from './PageNotFound/PageNotFound'
-export { default as BlogPage } from './Blog/BlogPage'
-export { default as ProjectPage } from './Project/ProjectPage'
