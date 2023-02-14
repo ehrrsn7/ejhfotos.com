8b7839edf94a77b94b17125456354da85e1b0c2e
E Harrison <har16020@byui.edu>
2023-02-13 17:05:43 -0800

    fixed linktree spacing issues, overflow-x should always be hidden now (just needs styling and then it's pretty much there)

--- linktree/src/App.jsx
+++ linktree/src/App.jsx
@@ -52,45 +52,28 @@ const links = [
 	},
 ]
 
+const profilePicture = "https://lh3.googleusercontent.com/gBBcfcWQX6hG1Azxve5mzgaEkjTjwNtFWJylOjkITwL3EbbLzq3MJfqGlnXT2hhf9NpWm73RIB_aWfcJ5hFwdjfehbXtniKBzPTKc4SgMy2KyPI_if8et5Q4eZ6qLNnd6N2GthPEEV8WnQcpsKSNgFpPZs3dfaH7mtyV0Aa2Dc6YJCj1zWXOPqAHuRgLYZRNeIDnKt20N_ntinty767IibQ_F7ufLnccgzmpo1KK1a4s3iHcdTWW69ZMX9W6k3YbUF6y3pGLS48WHZuD71ALjOlqO-iF_QAWprwxQUTfmgWN1PvPVAZYQpk4wC6MldHLCzoc-4joH_YmkMIzQ069bzLq0h7z7JpwmFtfGxGsK19OpgsgJr009C7zD3c1rnv82KMg71qs0mJvh2KSpqLExM0byvFRdj1lAZLwF6IcVGYn8PzaS3ssDtNXBRUvAj3hdvQXu1sBoU_lb1ncmplSb_RPiK1askrTtaw-ph5awVxAlLnrJRNaXB6vTRd637UnE1SDVRCnDFhjdd1_XVm7du7ugfm6FdbHp51FYCajWlZoIaslu9eGLOiqt06QJiF-wRbb7KjdGBNsj2TAmbGNRqRl-8Bb2LYiKSyl6E1MoY0eHOcExYHpmaKTCg6c9s6VGddZr3Rm42l2uWPVGCJ5rcC74FAt7xLTJayeiunFmWPViPbhet5sPmhqkhiRrbHTrZJT2jtD_xCgI1kM0i7E7jWSfvUbNWLexJTm4O6dUX2P60BoTxw-eSAqZKN5O92P0nxtLC7EBt0GN1ic70hVGOS4cIyWWepNC3pkcdhaDgI6eruF13YxmKJcsaVVONga_p7NckYXeHW5c9Io6J1e7J9xeEcko8-5FfBN9sMUC__xNE6kjcU2qUSjUwDgkhBhtsWKW3Pq3AXnbkrc-Jo5Vbx6oA4yu2gdlAVJBg6g0P7vHxcvWkxTt9qzBME5U7Xbd9RLlbzKsXMPZq1h88o=w746-h498-s-no?authuser=0"
+
 export function Header() {
-	return <header style={{
-		width: "400px",
-		textAlign: "center"
-	}}>
+	return <header id="Header">
 		<h1>Elijah Harrison</h1>
-		<img src="https://lh3.googleusercontent.com/gBBcfcWQX6hG1Azxve5mzgaEkjTjwNtFWJylOjkITwL3EbbLzq3MJfqGlnXT2hhf9NpWm73RIB_aWfcJ5hFwdjfehbXtniKBzPTKc4SgMy2KyPI_if8et5Q4eZ6qLNnd6N2GthPEEV8WnQcpsKSNgFpPZs3dfaH7mtyV0Aa2Dc6YJCj1zWXOPqAHuRgLYZRNeIDnKt20N_ntinty767IibQ_F7ufLnccgzmpo1KK1a4s3iHcdTWW69ZMX9W6k3YbUF6y3pGLS48WHZuD71ALjOlqO-iF_QAWprwxQUTfmgWN1PvPVAZYQpk4wC6MldHLCzoc-4joH_YmkMIzQ069bzLq0h7z7JpwmFtfGxGsK19OpgsgJr009C7zD3c1rnv82KMg71qs0mJvh2KSpqLExM0byvFRdj1lAZLwF6IcVGYn8PzaS3ssDtNXBRUvAj3hdvQXu1sBoU_lb1ncmplSb_RPiK1askrTtaw-ph5awVxAlLnrJRNaXB6vTRd637UnE1SDVRCnDFhjdd1_XVm7du7ugfm6FdbHp51FYCajWlZoIaslu9eGLOiqt06QJiF-wRbb7KjdGBNsj2TAmbGNRqRl-8Bb2LYiKSyl6E1MoY0eHOcExYHpmaKTCg6c9s6VGddZr3Rm42l2uWPVGCJ5rcC74FAt7xLTJayeiunFmWPViPbhet5sPmhqkhiRrbHTrZJT2jtD_xCgI1kM0i7E7jWSfvUbNWLexJTm4O6dUX2P60BoTxw-eSAqZKN5O92P0nxtLC7EBt0GN1ic70hVGOS4cIyWWepNC3pkcdhaDgI6eruF13YxmKJcsaVVONga_p7NckYXeHW5c9Io6J1e7J9xeEcko8-5FfBN9sMUC__xNE6kjcU2qUSjUwDgkhBhtsWKW3Pq3AXnbkrc-Jo5Vbx6oA4yu2gdlAVJBg6g0P7vHxcvWkxTt9qzBME5U7Xbd9RLlbzKsXMPZq1h88o=w746-h498-s-no?authuser=0" width="200px" alt="profile picture" style={{
-			clipPath: "circle(35%)"
-		}} />
+		<img id="ProfilePicture" src={profilePicture} alt="profile picture" />
 		<h3>Linktree</h3>
 	</header>
 }
 
-export function Footer() {
-	return <footer>
-		<p>Made by Elijah Harrison using React.js</p>
-		<span style={{display: "flex"}}>
-			<a href="http://ejhfotos.com/" style={{padding: "1em"}}><p>ejhfotos.com</p></a>
-			<a href="http://about.ejhfotos.com/" style={{padding: "1em"}}><p>about.ejhfotos.com</p></a>
-		</span>
-	</footer>
-}
-
-export default function App() {
-  	return <div id="App">
-      <Header />
-      <div>
-			{links.map(l => <a key={l.name} href={l.url}>
-				<div>
-					<button style={{margin: 0, padding: 0, paddingLeft: "1em", paddingRight: "1em", marginBottom: "1em"}}>
+export function Body() {
+	return <div id="Body">
+		{links.map(l => <a key={l.name} href={l.url} style={{width: "100%"}}>
+			<div>
+				<button style={{display: "block", margin: "0 0 1em 0", padding: "0 1em 0 1em", width: "100%"}}>
 					<span style={{
 						display: "flex",
 						flexDirection: "row",
 						placeItems: "center",
-						width: "400px",
+						flexWrap: "wrap",
 					}}>
-						<img src={l.icon} alt="instagram logo" width="20px" style={{
-							paddingRight: "1em", paddingTop: "-1em"
-						}} />
+						<img src={l.icon} alt="instagram logo" />
 						
 						<p>
 							{l.name}
@@ -102,10 +85,26 @@ export default function App() {
 							{l.user}
 						</p>
 					</span>
-					</button>
-				</div>
-			</a>)}
-		</div>
+				</button>
+			</div>
+		</a>)}
+	</div>
+}
+
+export function Footer() {
+	return <footer id="Footer">
+		<p>Made by Elijah Harrison using React.js</p>
+		<span style={{display: "flex"}}>
+			<a href="http://ejhfotos.com/" style={{padding: "1em"}}><p>ejhfotos.com</p></a>
+			<a href="http://about.ejhfotos.com/" style={{padding: "1em"}}><p>about.ejhfotos.com</p></a>
+		</span>
+	</footer>
+}
+
+export default function App() {
+  	return <div id="App">
+      <Header />
+      <Body />
       <Footer />
   	</div>
 }
