9eecbda2008073fc8f7d68c387003fb411ae0747
E Harrison <har16020@byui.edu>
2023-06-10 23:36:49 -0700

    made an assets alias to simplify imports

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
+import path from "path"
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-   host: "0.0.0.0"
-  }
+   plugins: [react()],
+   server: {
+      host: "0.0.0.0"
+   },
+   resolve: {
+      alias: [
+         {
+				find: "@assets",
+				replacement: path.resolve(__dirname, "src/assets")
+			},
+      ]
+   },
 })
